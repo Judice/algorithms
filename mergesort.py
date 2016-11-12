14460005ce5247743e553867a3034096f62e251b
Judice <Lancelot@zhengjiandeMacBook-Pro.local>
2016-11-12 16:39:12 +0800

    guibingpaixun

---
+++ mergesort.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+def merge_sort(seq):
+    mid = len(seq)//2
+    lft, rgt = seq[:mid], seq[mid:]
+    if len(lft)>1:
+        lft = merge_sort(lft)
+    if len(rgt)>1:
+        rgt = merge_sort(rgt)
+    res = []
+    while lft and rgt:
+        if lft[-1] >= rgt[-1]:
+            res.append(lft.pop())
+        else:
+            res.append(rgt.pop())
+    res.reverse()
+    return   res
+
+nums = [78,4,2,6,3,5,9,7,8,13,11,42,26,78,58,49,99,37,44,66,61,88]
+print 'final:', merge_sort(nums)
